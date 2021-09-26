bd6422b1d2caaa06730c5f4f9c0a3e798685be46
Jens Møller <jensz123@gmail.com>
2021-09-26 13:51:43 +0200

    Update Program.cs

--- converter/Program.cs
+++ converter/Program.cs
@@ -1,28 +1,87 @@
-﻿using System;
+using System;
 
 namespace converter
 {
     class Program
     {
-        static int readNumber()
-
+        static Double ReadDouble(string tekst)
         {
-            String[] input = new string[10];
-            int[] number = new int[10];
-            for (int i = 0; i < 10; i++)
-            {
-                string temp = Console.ReadLine();
-                number[i] = Convert.ToInt32(temp);
-            }
+            Console.Write(tekst);
+            String input = Console.ReadLine();
+            double valu = Convert.ToDouble(input);
+            return valu;
         }
+
         static void Main(string[] args)
         {
-          
-          foreach (int tal in number)
-          {
-              Console.WriteLine(tal);
-          }
 
+            Console.Clear();
+            Console.WriteLine("Omergen og Lomeregner");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("1. Lommeregner");
+            Console.WriteLine("2. Omregner");
+            Console.WriteLine("---------------------");
+            Console.Write("Vælg funktion: ");
+            String input = Console.ReadLine();
+
+            if (input == "1" || input == "Lomeregner")
+            {
+
+                double resultat = 0;
+                Console.Clear();
+                Console.WriteLine("---------------------");
+                Console.WriteLine("1. Plus");
+                Console.WriteLine("2. Træk fra");
+                Console.WriteLine("3. Gange");
+                Console.WriteLine("4. Dividere");
+                Console.WriteLine("---------------------");
+                Console.Write("Vælg funktion: ");
+                input = Console.ReadLine();
+
+                Double tal1 = ReadDouble("Tal 1: ");
+                Double tal2 = ReadDouble("Tal 2: ");
+
+                if (input == "1" || input == "+" || input == "Plus")
+                {
+                    resultat = tal1 + tal2;
+                }
+
+                if (input == "2" || input == "-" || input == "Træk fra")
+                {
+                    resultat = tal1 - tal2;
+                }
+
+                if (input == "3" || input == "*" || input == "Gange")
+                {
+                    resultat = tal1 * tal2;
+                }
+
+                if (input == "4" || input == "/" || input == "Dividere")
+                {
+                    resultat = tal1 / tal2;
+                }
+
+                Console.WriteLine("---------------------");
+                Console.Write("Resultat: ");
+                Console.WriteLine(resultat);
+                Console.WriteLine("---------------------");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------");
+                Console.WriteLine("1. Fra Disimaltal til Binærtal");
+                Console.WriteLine("2. Fra Disimaltal til Hex");
+                Console.WriteLine("3. Fra Hex til Binærtal");
+                Console.WriteLine("4. Fra Hex til Disimaltal");
+                Console.WriteLine("5. Fra Binærtal til Hex");
+                Console.WriteLine("6. Fra Binærtal til Disimaltal");
+                Console.WriteLine("---------------------");
+                Console.Write("Vælg funktion: ");
+                input = Console.ReadLine();
+
+            }
         }
     }
 }
