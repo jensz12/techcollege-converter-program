f4c3890d61b5294c63ef2458d75d0c011c24966e
Jens Møller <jensz123@gmail.com>
2021-09-26 11:42:53 +0200

    Add files via upload

--- converter/Program.cs
+++ converter/Program.cs
@@ -4,15 +4,25 @@ namespace converter
 {
     class Program
     {
+        static int readNumber()
 
-
-
+        {
+            String[] input = new string[10];
+            int[] number = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                string temp = Console.ReadLine();
+                number[i] = Convert.ToInt32(temp);
+            }
+        }
         static void Main(string[] args)
         {
-          //variabler
-          String[]
+          
+          foreach (int tal in number)
+          {
+              Console.WriteLine(tal);
+          }
 
-     
         }
     }
 }
