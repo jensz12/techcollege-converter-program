189106fb8591ca96ee30acf151574ed8897befc5
Jens Møller <jens34d2@elev.techcollege.dk>
2021-12-12 22:11:41 +0100

    Update Program.cs

--- converter/Program.cs
+++ converter/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace converter
+namespace Ider_til_opg
 {
     class Program
     {
@@ -110,8 +110,125 @@ namespace converter
         }
         static void Decimaltal_til_Hex()
         {
-            Decimaltal_til_Binærtal();
-            Binærtal_til_Hex();
+            int x = 128;
+            string[] binærtal = new string[8];
+            int[] hextal1 = new int[4];
+            int[] hextal2 = new int[4];
+            string Decimal = Console.ReadLine();
+            int Decimaltal = Convert.ToInt32(Decimal);
+            Console.Clear();
+            for (int a = 0; a != 8; a++)
+            {
+                if (Decimaltal > x || Decimaltal == x)
+                {
+                    binærtal[a] = "1";
+                    Decimaltal = Decimaltal - x;
+                    x = x / 2;
+
+                }
+                else
+                {
+                    binærtal[a] = "0";
+                    x = x / 2;
+                }
+            }
+
+            for (int i2 = 0; i2 < 4; i2++)
+            {
+                string[] hex1 = new string[4];
+                hex1[i2] = binærtal[i2];
+                hextal1[i2] = Convert.ToInt32(hex1[i2]);
+            }
+            int i4 = 0;
+            for (int i3 = 4; i3 < 8; i3++)
+            {
+                string[] hex2 = new string[i3];
+                hex2[i4] = binærtal[i3];
+                hextal2[i4] = Convert.ToInt32(hex2[i4]);
+                i4++;
+            }
+            x = 1;
+            for (int a = 0; a < 4; a++)
+            {
+                hextal1[a] = hextal1[a] * x;
+                x = x * 2;
+            }
+            x = 1;
+            for (int a = 0; a < 4; a++)
+            {
+                hextal2[a] = hextal2[a] * x;
+                x = x * 2;
+            }
+            string[] bogstaver = { "A", "B", "C", "D", "E", "f" };
+            int hexdi1 = hextal1[0] + hextal1[1] + hextal1[2] + hextal1[3];
+            int hexdi2 = hextal2[0] + hextal2[1] + hextal2[2] + hextal2[3];
+            if (hexdi1 > 9)
+            {
+                if (hexdi1 == 10)
+                    Console.Write(bogstaver[0]);
+
+                if (hexdi1 == 11)
+                    Console.Write(bogstaver[1]);
+
+                if (hexdi1 == 12)
+                    Console.Write(bogstaver[2]);
+
+                if (hexdi1 == 13)
+                    Console.Write(bogstaver[3]);
+
+                if (hexdi1 == 14)
+                    Console.Write(bogstaver[4]);
+
+                if (hexdi1 == 15)
+                    Console.Write(bogstaver[5]);
+            }
+
+            if (hexdi2 > 9)
+            {
+                if (hexdi2 == 10)
+                    Console.Write(bogstaver[0]);
+
+                if (hexdi2 == 11)
+                    Console.Write(bogstaver[1]);
+
+                if (hexdi2 == 12)
+                    Console.Write(bogstaver[2]);
+
+                if (hexdi2 == 13)
+                    Console.Write(bogstaver[3]);
+
+                if (hexdi2 == 14)
+                    Console.Write(bogstaver[4]);
+
+                if (hexdi2 == 15)
+                    Console.Write(bogstaver[5]);
+            }
+            Console.ReadKey();
+        }
+        static void Binærtal_til_Decimaltal()
+        {
+            int x = 128;
+
+            string[] binærtal = new string[8];
+            int[] tal = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                binærtal[i] = Console.ReadLine();
+
+            }
+            Console.Clear();
+            for (int a = 0; a < 8; a++)
+            {
+                tal[a] = Convert.ToInt32(binærtal[a]);
+            }
+            for (int b = 0; b < 8; b++)
+            {
+                tal[b] = tal[b] * x;
+                x = x / 2;
+            }
+            int ialt = tal[0] + tal[1] + tal[2] + tal[3] + tal[4] + tal[5] + tal[6] + tal[7];
+            Console.WriteLine(ialt);
+            Console.ReadKey();
         }
         static Double ReadDouble(string tekst)
         {
@@ -199,7 +316,7 @@ namespace converter
                     Console.WriteLine("Fra Decimaltal til Hex");
                     Console.WriteLine("Skriv et Decimaltal fra 0 til 255");
                     Console.WriteLine("---------------------");
-
+                    Decimaltal_til_Hex();
                 }
                 if (input == "5")
                 {
@@ -208,6 +325,14 @@ namespace converter
                     Console.WriteLine("Skriv et binærtal fra 0 til 1, 8 gange  ");
                     Console.WriteLine("---------------------");
                     Binærtal_til_Hex();
+                }
+                if (input == "6")
+                {
+                    Console.WriteLine("---------------------");
+                    Console.WriteLine("Fra Binærtal til Decimaltal ");
+                    Console.WriteLine("Skriv et binærtal fra 0 til 1, 8 gange  ");
+                    Console.WriteLine("---------------------");
+                    Binærtal_til_Decimaltal();
                 }
             }
         }
